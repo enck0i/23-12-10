504d9b0569ad179b6a10e36326a0524665f3d5fa
3501015766@qq.com <3501015766@qq.com>
2023-12-10 08:57:04 +0800

    添加项目文件。

---
+++ 23-12-10/源.c
@@ -0,0 +1,21 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//输入一个三位数和一个两位数，并输出他们相乘的竖式
+int main()
+{
+	int abc;
+	int de;
+	int	i = 1;
+	scanf("%d", &abc);
+	while (scanf("%d", &de) == 1)
+	{
+		printf("<%d>\n", i++);
+		printf("..%d\nX..%d\n", abc, de);
+		printf("-----\n");
+		printf(".%d\n", abc * (de % 10));
+		printf("%d.\n", abc * (de / 10));
+		printf("-----\n");
+		printf("%d\n\n", abc * (de % 10) + abc * (de / 10) * 10);
+	}
+	return 0;
+}
